7b43531c6bd83431a7eda6df8ef289f307a7016c
sahil2501 <82953793+sahil2501@users.noreply.github.com>
2021-07-10 13:11:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "grey";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+ctx.beginPath();
+color = "blue";
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+color = "black";
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(350, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+color = "red";
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(450, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+color = "orange";
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(300, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+color = "green";
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(400, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
